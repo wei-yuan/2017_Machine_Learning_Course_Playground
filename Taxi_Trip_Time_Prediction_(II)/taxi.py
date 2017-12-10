4cae517d4e4d7ba9f360bb037fcd8a8e169b66b9
wei-yuan <ax61316@hotmail.com>
2017-12-10 15:02:38 +0800

    Add hw3 taxi ECML/PKDD 15: Taxi Trip Time Prediction (II)

---
+++ Taxi_Trip_Time_Prediction_(II)/taxi.py
@@ -0,0 +1,15 @@
+# --------------------------------------
+# Library
+# --------------------------------------
+# Tools
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import graphviz
+
+# Model
+from sklearn.neural_network import MLPClassifier
+
+# Validation tool
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
