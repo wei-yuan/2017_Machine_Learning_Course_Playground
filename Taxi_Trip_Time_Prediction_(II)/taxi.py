ab93d188a748b06b2e8116dbf32a981730dc0c07
wei-yuan <ax61316@hotmail.com>
2017-12-17 13:19:00 +0800

    Finish a linear regression model

--- Taxi_Trip_Time_Prediction_(II)/taxi.py
+++ Taxi_Trip_Time_Prediction_(II)/taxi.py
@@ -3,17 +3,14 @@
 # --------------------------------------
 # Tools
 from pandas import read_csv
-from pandas import DataFrame
-from pandas import concat
 import numpy
 import math
-from datetime import datetime
-import tzlocal
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+from sklearn import linear_model
 
 # Model
 from sklearn.neural_network import MLPClassifier
@@ -22,63 +19,54 @@ from keras.layers import Dense
 from keras.layers import LSTM
 
 # Validation tool
+from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import mean_squared_error
-from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn.model_selection import cross_val_score
 
 ########################
 #---- Load dataset ----#
 ########################
-from pandas import read_csv
 dataset = read_csv('reordered.csv')
+print (dataset.head(5))
 values  = dataset.values
 
 ########################
 #-Train / Test dataset-#
 ########################
 # split into train and test sets
-n_features = 3
-n_data = 8
-train = values[:n_data, :n_features] # :n_features = 0 : n_train_hours
-test = values[n_data:, :]
+n_data = len(dataset.index) # total number of data
+train_fields = ['HOUR', 'CALL_TYPE', 'TAXI_ID']
+train = read_csv('reordered.csv', usecols=train_fields)
+label_fields = ['POLYLINE']
+label = read_csv('reordered.csv', usecols=label_fields)
 # split into input and outputs
 #n_obs = n_data * n_features
 
-train_X, train_y = train[:, :n_obs], train[:, -n_features]
-test_X, test_y = test[:, :n_obs], test[:, -n_features]
-print(train_X.shape, len(train_X), train_y.shape)
-# reshape input to be 3D [samples, timesteps, features]
-train_X = train_X.reshape((train_X.shape[0], n_hours, n_features))
-test_X = test_X.reshape((test_X.shape[0], n_hours, n_features))
-print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
-
+x_train, x_test, y_train, y_test = train_test_split(
+									train, 
+									label, 
+									test_size=0.4, 
+									random_state=0)
 
 ########################
 #------- Model --------#
 ########################
-# design network
-def linear_regression_model():
-	# create model
-	model = Sequential()
-	model.add(Dense(13, input_dim=13, kernel_initializer='normal', activation='relu'))
-	model.add(Dense(1, kernel_initializer='normal'))
-	# Compile model
-	model.compile(loss='mean_squared_error', optimizer='adam')
-	return model
+regr = linear_model.LinearRegression()
+regr.fit(x_train, y_train)
 
 ########################
 #------Evaluation------#
 ########################
+# evaluate model
+y_pred = regr.predict(x_test)
+#diabetes_y_pred = regr.predict(diabetes_X_test)
+print ("accuracy: %.2f" % regr.score(x_test, y_test))
+print("Mean squared error: %.2f"
+      % mean_squared_error(y_test, y_pred))
 
-# evaluate model with standardized dataset
-estimator = KerasRegressor(build_fn=linear_regression_model, nb_epoch=100, batch_size=200, verbose=0)
-
-
-kfold = KFold(n_splits=10)
-results = cross_val_score(estimator, X, Y, cv=kfold)
-print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+#scores = cross_val_score(clf, iris.data, iris.target, cv=5)
 
 '''
 model = Sequential()
