5a7e71ecb19c7423cb6aea589c90e0a918dfac58
wei-yuan <ax61316@hotmail.com>
2017-12-18 08:39:06 +0800

    Finish logistic regression model

--- mnist/mnist_logistic_regression.py
+++ mnist/mnist_logistic_regression.py
@@ -1,49 +1,55 @@
-
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
+from keras.datasets import mnist
 from keras.utils import np_utils
-# mnist dataset
-from keras.datasets import mnist 
-from keras.models import Sequential 
-from keras.layers import Dense, Activation 
 
-# import data
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+batch_size = 128
+nb_classes = 10
+nb_epoch = 10
+
 ######################
 # data preprocessing #
 ######################
-input_dim = 784 # 28 * 28 to  784 * 1 
-X_train = X_train.reshape(60000, input_dim) 
-X_test = X_test.reshape(10000, input_dim) 
-# convert to float
-X_train = X_train.astype('float32') 
-X_test = X_test.astype('float32') 
-
-X_train /= 255 
+# Load MNIST dataset
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = X_train.reshape(60000, 784)
+X_test = X_test.reshape(10000, 784)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
 X_test /= 255
+Y_Train = np_utils.to_categorical(y_train, nb_classes)
+Y_Test = np_utils.to_categorical(y_test, nb_classes)
 
-# change original data to
-nb_classes = 10  # 10 digit class
-Y_train = np_utils.to_categorical(y_train, nb_classes) 
-Y_test = np_utils.to_categorical(y_test, nb_classes)
-
+def show_train_history(train_history, train, validation):
+    plt.plot(train_history.history[train])
+    plt.plot(train_history.history[validation])
+    plt.title('Train History')
+    plt.ylabel(train)
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
 ######################
 ####### Model ########
 ######################
-output_dim = nb_classes
-model = Sequential() 
-model.add(Dense(output_dim, input_dim=input_dim, activation='softmax')) 
-batch_size = 128 
-nb_epoch = 20
+# Logistic regression model
+model = Sequential()
+model.add(Dense(output_dim=10, input_shape=(784,), init='normal', activation='softmax'))
+#model.compile(optimizer=SGD(lr=0.05), loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+# check model
+model.summary()
 
 ######################
 ####### Train  #######
 ######################
-model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy']) 
-history = model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,verbose=1, validation_data=(X_test, Y_test)) 
+history = model.fit(X_train, Y_Train, nb_epoch=nb_epoch, batch_size=batch_size, verbose=1)
 
 ######################
 ##### Evaluation #####
 ######################
-
-score = model.evaluate(X_test, Y_test, verbose=0) 
-print('Test score:', score[0]) 
-print('Test accuracy:', score[1])
+evaluation = model.evaluate(X_test, Y_Test, verbose=1)
+print('Summary: Loss over the test dataset: %.2f, Accuracy: %.2f' % (evaluation[0], evaluation[1]))
