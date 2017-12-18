e34efd82b8d45c946e75ea52051122e1f79dc598
wei-yuan <ax61316@hotmail.com>
2017-12-18 08:55:32 +0800

    Finish plot of logistic regression

--- mnist/mnist_logistic_regression.py
+++ mnist/mnist_logistic_regression.py
@@ -1,27 +1,38 @@
+# dataset
+from keras.datasets import mnist
+# model
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import SGD
-from keras.datasets import mnist
+# lib
 from keras.utils import np_utils
 
-batch_size = 128
+# tool
+# %matplotlib inline
+import matplotlib.pyplot as plt
+
+# parametter
 nb_classes = 10
 nb_epoch = 10
+batch_size = 256
 
+# Load MNIST dataset
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
 ######################
 # data preprocessing #
 ######################
-# Load MNIST dataset
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
 X_train = X_train.reshape(60000, 784)
 X_test = X_test.reshape(10000, 784)
+# convert to float
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
+# normalization
 X_train /= 255
 X_test /= 255
+# change to category
 Y_Train = np_utils.to_categorical(y_train, nb_classes)
 Y_Test = np_utils.to_categorical(y_test, nb_classes)
-
+# plot
 def show_train_history(train_history, train, validation):
     plt.plot(train_history.history[train])
     plt.plot(train_history.history[validation])
@@ -46,10 +57,12 @@ model.summary()
 ######################
 ####### Train  #######
 ######################
-history = model.fit(X_train, Y_Train, nb_epoch=nb_epoch, batch_size=batch_size, verbose=1)
+train_history = model.fit(X_train, Y_Train, nb_epoch=nb_epoch, batch_size=batch_size, validation_split=0.2, verbose=1)
 
 ######################
 ##### Evaluation #####
 ######################
 evaluation = model.evaluate(X_test, Y_Test, verbose=1)
 print('Summary: Loss over the test dataset: %.2f, Accuracy: %.2f' % (evaluation[0], evaluation[1]))
+show_train_history(train_history, 'acc', 'val_acc')
+show_train_history(train_history, 'loss', 'val_loss')
